066f589f13d6854a4e93934581d2556131dbbc38
dhirender <kumardhirender04@gmail.com>
2022-01-31 17:09:01 +0530

    add secury code and key

--- app/src/main/java/com/app/wallpaperwish/ui/WallpaperListing.kt
+++ app/src/main/java/com/app/wallpaperwish/ui/WallpaperListing.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
 import com.app.wallpaperwish.R
 import com.app.wallpaperwish.adapters.WallpaperListingAdapter
+import com.unsplash.pickerandroid.photopicker.UnsplashPhotoPicker
+import com.unsplash.pickerandroid.photopicker.presentation.UnsplashPickerActivity
 import kotlinx.android.synthetic.main.activity_wallpaper_listing.*
 
 class WallpaperListing : AppCompatActivity() {
@@ -15,6 +17,28 @@ class WallpaperListing : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wallpaper_listing)
 
+
+
+
+
+        UnsplashPhotoPicker.init(
+            application, // application
+            "dAsC4eDB3l52Yc9SkFYjhZ8a6jcUl-zn3u17K_Zm3sg",
+            "-hkfGFK9ym-uiRAiKM5_vDdNVwrpY4QIQUuwcp8IJ78"
+            /* optional page size */
+        )
+
+
+
+        startActivityForResult(
+            UnsplashPickerActivity.getStartingIntent(
+                this, // context
+                isMultipleSelection
+            ), REQUEST_CODE
+        )
+
+
+
         //color listing
         list.add(resources.getColor(R.color.black))
         list.add(resources.getColor(R.color.blue))
@@ -23,11 +47,7 @@ class WallpaperListing : AppCompatActivity() {
         list.add(resources.getColor(R.color.red))
         list.add(resources.getColor(R.color.yellow))
         setAdapter()
-
     }
-
-
-
     private  fun setAdapter(){
         val adapterView = WallpaperListingAdapter()
         rvWallpaperListing.adapter = adapterView
